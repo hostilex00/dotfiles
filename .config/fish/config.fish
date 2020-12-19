3f2e7984a0c37158118e7e2b070505238774bbac
hostile <hostile.hacking@gmail.com>
2020-12-19 15:48:22 +0100

    Add config files

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+starship init fish | source
+
+set fish_greeting ""
+# --- Fish colors
+set fish_color_command 'brcyan'
+set fish_color_error 'brred'
+set fish_color_param 'brblue'
+set fish_color_quote 'brgreen'
+set fish_color_search_match --background='blue'
+set fish_color_operator 'brgreen'
+set fish_color_end 'brgreen'
+set fish_color_redirection 'brgreen'
+set color_prompt 'brblack'
+set fg_color 'cyan'
